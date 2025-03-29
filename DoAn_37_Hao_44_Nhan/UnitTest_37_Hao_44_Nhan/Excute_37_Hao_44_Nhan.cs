ca76ee5aeec2baf08cc23d4280c0a0320bbe0f97
haothach <haonhutthach10@gmail.com>
2025-03-29 18:29:22 +0700

    add

---
+++ DoAn_37_Hao_44_Nhan/UnitTest_37_Hao_44_Nhan/Excute_37_Hao_44_Nhan.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace UnitTest_37_Hao_44_Nhan
+{
+    [TestClass]
+    public class Excute_37_Hao_44_Nhan
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ChromeDriverService chrome = ChromeDriverService.CreateDefaultService();
+            chrome.HideCommandPromptWindow = true;
+
+            IWebDriver driver = new ChromeDriver(chrome);
+            driver.Navigate().GoToUrl("https://www.fahasa.com/");
+            Thread.Sleep(1000);
+            IWebElement clear = driver.FindElement(By.ClassName("fhs_top_account_button"));
+            clear.Click();
+            Thread.Sleep(1000);
+            IWebElement user = driver.FindElement(By.Id("login_username"));
+            user.SendKeys("0335607157");
+            Thread.Sleep(1000);
+            IWebElement pass = driver.FindElement(By.Id("login_password"));
+            pass.SendKeys("T123456789");
+            Thread.Sleep(1000);
+            IWebElement login = driver.FindElement(By.ClassName("fhs-btn-login"));
+            login.Click();
+
+
+        }
+    }
+}
---
+++ DoAn_37_Hao_44_Nhan/UnitTest_37_Hao_44_Nhan/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest_37_Hao_44_Nhan")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest_37_Hao_44_Nhan")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a4e366a0-4aca-43af-9b26-34d87c028e0a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
